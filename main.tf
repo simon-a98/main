07979ed265db74d3aa03058b7a3c5cf294320db0
simon-a98 <falcon.bluev398@gmail.com>
2025-07-17 14:27:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,89 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "jenkins-vpc"
+  }
+}
+
+# Create a subnet
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "jenkins-subnet"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "jenkins-gw"
+  }
+}
+
+# Create a route table
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "jenkins-rt"
+  }
+}
+
+# Associate route table with subnet
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# Create a security group
+resource "aws_security_group" "nginx_sg" {
+  name        = "nginx-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nginx-sg"
+  }
+}
+
+# Launch EC2 instance in the VPC
 resource "aws_instance" "nginx_server" {
-  ami           = "ami-03f4878755434977f" # Amazon Linux 2 AMI for ap-south-1
-  instance_type = "t2.micro"
+  ami                    = "ami-03f4878755434977f"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.nginx_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -18,5 +98,6 @@ resource "aws_instance" "nginx_server" {
     Name = "nginx-jenkins-instance"
   }
 }
+
 
 
